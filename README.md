d6953f6232cac86c68f3dfd1dc379b7af44bc291
Monserrat <monserratdiaz12@gmail.com>
2017-04-03 12:32:07 +0000

    wrote functions and started README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # calculatorStarterCode
+
+## This project is a calculator, it takes an input and does some sort of math operation to give an output
+
+for example, if you input a 9 by clicking on the corresponding number, then clicked the sqrt button, the calculator 
+would give you the square root of 9, an output of 3.
--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -1,45 +1,45 @@
 // All the code you will need to create for this project will be completed below
 // Your functions must be called the following:
     // multiply, divide, subtract, add, power, square_root, logBase10, generateRandomNumber
-
+/*global math*/
 
 function divide(x,y)
 {
 	//Write your code below this line
-	
+	return x/y;
 }
 
 function multiply(x,y)
 {
-	
+	return x*y;
 }
 
 function subtract(x,y)
 {
-	
+	return x-y;
 }
 
 function add(x,y)
 {
-	
+	return x+y;
 }
 
 function power(x,y)
 {
-	
+	return Math.pow(x,y);
 }
 
 function square_root(x)
 {
-	
+    return Math.sqrt(x);	
 }
 
 function logBase10(x)
 {
-	
+	return Math.log10(x);
 }
 
-function generateRandomNumber()
+function generateRandomNumber(x,y)
 {
-	
+    return Math.floor(Math.random() * (x - y)) + y;	
 }
